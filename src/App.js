e99d434b7dce2b9a60583770134e06ecd01d904e
krystiano13 <crystianotv@gmail.com>
2023-09-30 20:31:43 +0200

    configure communication between react and redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
+    const todos = useSelector(state => state.todos);
+    const dispatch = useDispatch();
+
     return (
         <div>
             <h1>Redux TO-DO App</h1>
+            <ul>
+                {
+                    todos.map(item => (
+                        <p>{ item.name }</p>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
--- src/reducers/todoSlice.js
+++ src/reducers/todoSlice.js
@@ -1,24 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: []
-}
-
-export const todoSlice = createSlice(
+  todos: [
     {
-        name: "todo",
-        initialState: initialState,
-        reducers: {
-            add: (state, action) => {
-                state.todos.push(action.payload);
-            },
+      id: 0,
+      name: "Learn Redux Toolkit",
+    },
+  ],
+};
 
-            remove: (state, action) => {
-                state.todos.filter(item => item.id !== action.payload);
-            }
-        }
-    }
-);
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: initialState,
+  reducers: {
+    add: (state, action) => {
+      state.todos.push({
+        id: state.todos[state.todos.length - 1].id + 1,
+        name: action.payload,
+      });
+    },
+
+    remove: (state, action) => {
+      state.todos.filter((item) => item.id !== action.payload);
+    },
+  },
+});
 
 export const { add, remove } = todoSlice.actions;
 export default todoSlice.reducer;
